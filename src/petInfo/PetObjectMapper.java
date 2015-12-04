cba9c9c2fa3b671b0933bba242d1676023a2da9e
bhowland <benjhowland@gmail.com>
2015-12-04 16:37:56 -0600

    worked on  mapper and still got to the same place as was last night

--- src/petInfo/PetObjectMapper.java
+++ src/petInfo/PetObjectMapper.java
@@ -17,28 +17,26 @@ public class PetObjectMapper {
 
         public void addPet(String postBodyData) {
 
-            System.out.println("IN PetObjectMapper 1: " + postBodyData);
-            OverallPets petsGeneral = JsonMapper.decode(postBodyData, OverallPets.class);
+            System.out.println("IN PetObjectMapper before swapping: " + postBodyData);
+            OverallPets overallPets = JsonMapper.decode(postBodyData, OverallPets.class);
             PetsArray petsArray = PetsArray.getInstance();
-            System.out.println("IN PetObjectMapper 2: " + petsArray.getPetsArray());
-            petsArray.addPet(petsGeneral);
-            System.out.println("IN PetObjectMapper 3: " + petsArray.getPetsArray());
+            petsArray.addPet(overallPets);
+            System.out.println("IN PetObjectMapper after swapping: " + petsArray);
+
 
 
             int i = 0;
             for (OverallPets p : petsArray.getPetsArray().get(PetsArray.KEY)) {
-
-
-                System.out.println("ATDB: For loop: ran " + i + " time(s)");
-                System.out.println("ATDB: For loop: " + p.getList().get(98).getName().toString());
-                p.getList().get(i).getCanBattle();
-                p.getList().get(i).getName();
-                p.getList().get(i).getFamily();
-                p.getList().get(i).getStrongAgainst();
-                p.getList().get(i).getWeakAgainst();
-                addOrUpdate.addOrUpdatePet(p);
-                i++;
+                    System.out.println("ATDB: For loop: " + p);
+                    System.out.println("ATDB: For loop: " + petsArray);
+                    System.out.println("Loaded a total of " + i + " pets");
+                    p.getList().get(i).getCanBattle();
+                    p.getList().get(i).getName();
+                    p.getList().get(i).getFamily();
+                    p.getList().get(i).getStrongAgainst();
+                    p.getList().get(i).getWeakAgainst();
+                    addOrUpdate.addOrUpdatePet(p);
+                    i++;
             }
         }
-
 }
